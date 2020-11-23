7474277b069f3539d1de3420036ab5b9760d6ac0
Ärthem O <oarthemlive@gmail.com>
2020-11-23 23:36:49 +0300

    after_review

--- js/script.js
+++ js/script.js
@@ -3,12 +3,15 @@ const profileSubtitle = document.querySelector('.profile__credentials-subtitle')
 const editBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const сlosePopupBtn = document.querySelector('.popup__close-button');
-const popupField = document.querySelectorAll('.popup__field');
+const popupFieldnickname = document.querySelector('[name="nickname"]');
+const popupFieldinfo = document.querySelector('[name="info"]');
 const formElement = document.querySelector('.popup__container');
 
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  popupFieldnickname.value = profileTitle.textContent;
+  popupFieldinfo.value = profileSubtitle.textContent;
 }
 editBtn.addEventListener('click', openPopup);
 
@@ -18,16 +21,14 @@ function closePopup() {
 }
 сlosePopupBtn.addEventListener('click', closePopup);
 
-popupField[0].value = profileTitle.textContent;
-popupField[1].value = profileSubtitle.textContent;
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let nameInput = popupField[0].value;
+    let nameInput = popupFieldnickname.value;
     profileTitle.textContent = nameInput;
-    let jobInput = popupField[1].value;
+    let jobInput = popupFieldinfo.value;
     profileSubtitle.textContent = jobInput;
 
     closePopup()
